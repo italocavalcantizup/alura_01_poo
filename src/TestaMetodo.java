78879af302a7b5f38f1418ccf47c01784bfbff4b
Italo cavalcanti <italocavalcanti@ZUP-6407.local>
2022-09-13 16:59:34 -0300

    criando classe Cliente

--- src/TestaMetodo.java
+++ src/TestaMetodo.java
@@ -8,24 +8,26 @@ public class TestaMetodo {
 		
 		System.out.println(conta1.getSaldo());
 		
-		boolean conseguiuRetirar = conta1.saca(300);
+		conta1.saca(300);
 		System.out.println();
-		
 		
 		Conta conta2 = new Conta();
 		conta2.deposita(600);
 		
-		System.out.println("Testando transferência");
-		System.out.println("Saldo 1: " + conta1.getSaldo());
-		System.out.println("Saldo 2: " + conta2.getSaldo());
+//		System.out.println("Testando transferência");
+//		System.out.println("Saldo 1: " + conta1.getSaldo());
+//		System.out.println("Saldo 2: " + conta2.getSaldo());
 		
 		conta1.transfere(150, conta2);
-		System.out.println("Saldo 1: " + conta1.getSaldo());
-		System.out.println("Saldo 2: " + conta2.getSaldo());
+//		System.out.println("Saldo 1: " + conta1.getSaldo());
+//		System.out.println("Saldo 2: " + conta2.getSaldo());
 		
 		conta2.transfere(500, conta1);
-		System.out.println("Saldo 1: " + conta1.getSaldo());
-		System.out.println("Saldo 2: " + conta2.getSaldo());
+//		System.out.println("Saldo 1: " + conta1.getSaldo());
+//		System.out.println("Saldo 2: " + conta2.getSaldo());
+		
+		conta1.setTitular("Italo Cavalcanti");
+		conta2.setTitular("Mariana Costa");
 		
 	}
 
